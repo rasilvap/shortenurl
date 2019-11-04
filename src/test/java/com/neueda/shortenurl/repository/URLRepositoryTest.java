752011ad22c7db57391c981aa6408866c1513b14
Rodolfo Alexander Silva Pantoja <rodolfo.silva@globant.com>
2019-11-04 14:57:06 -0500

    RS-shortenurl: Added URLRepository test.

--- src/test/java/com/neueda/shortenurl/repository/URLRepositoryTest.java
+++ src/test/java/com/neueda/shortenurl/repository/URLRepositoryTest.java
@@ -1,34 +1,64 @@
 package com.neueda.shortenurl.repository;
 
+import ai.grakn.redismock.RedisServer;
+import com.neueda.shorturl.exception.URLNotFoundException;
 import com.neueda.shorturl.repository.ShortURLRepository;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.apache.commons.lang.RandomStringUtils;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.data.redis.connection.RedisServer;
+import org.mockito.Mockito;
 import redis.clients.jedis.Jedis;
 
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 
 public class URLRepositoryTest {
-    private static RedisServer server;
-    @BeforeClass public static void setupServer() throws IOException {
-        server = RedisServer.newServerFrom(6789);
+    public static final String ID_KEY = "id";
+    public static final String URL_KEY = "url:";
+    private RedisServer server;
+    private Jedis mockJedis;
+    
+    @Before
+    public void setupServer() throws IOException {
+        mockJedis = Mockito.mock(Jedis.class);
+        server = RedisServer.newRedisServer(6789);
         server.start();
     }
     
-    @AfterClass public static void shutdownServer() throws IOException {
+    @After
+    public void shutdownServer() throws IOException {
         server.stop();
     }
     
     @Test
-    public void test_incrementID_StartsAt0AndIncrements() {
-        ShortURLRepository urlRepository = new ShortURLRepository(new Jedis(server.getHost(), server.getPort())
-                , "id", "url:");
+    public void test_incrementID_StartsAt0AndIncrementsByOne() {
+        ShortURLRepository urlRepository = new ShortURLRepository(new Jedis(server.getHost(), server.getBindPort())
+                , ID_KEY, URL_KEY);
         for (long expectedId = 0L; expectedId < 50L; ++expectedId) {
             long actualId = urlRepository.incrementID();
             assertEquals(expectedId, actualId);
         }
+    }
+    
+    @Test
+    public void getUrl_returnUrlFromStore() throws Exception {
+        String expectedUrl = RandomStringUtils.randomAlphabetic(10);
+        mockJedis = Mockito.mock(Jedis.class);
+        given(mockJedis.hget(anyString(), anyString())).willReturn(expectedUrl);
+        ShortURLRepository urlRepository = new ShortURLRepository(mockJedis, ID_KEY, URL_KEY);
+        String actuaUrl = urlRepository.getUrl(Long.valueOf(RandomStringUtils.randomNumeric(3)));
+        assertEquals(expectedUrl, actuaUrl);
+    }
+    
+    @Test(expected = URLNotFoundException.class)
+    public void getUrl_doesNotExist__throwException() throws Exception {
+        mockJedis = Mockito.mock(Jedis.class);
+        given(mockJedis.hget(anyString(), anyString())).willReturn(null);
+        ShortURLRepository urlRepository = new ShortURLRepository(mockJedis, ID_KEY, URL_KEY);
+        urlRepository.getUrl(Long.valueOf(RandomStringUtils.randomNumeric(3)));
     }
 }
