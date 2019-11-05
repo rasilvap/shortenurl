38e967b8b1de05043dd47fe78c1248428b248c9e
Rodolfo Alexander Silva Pantoja <rodolfo.silva@globant.com>
2019-11-04 22:40:21 -0500

    RS-shortenurl: Typo issues fixed.

--- src/main/java/com/neueda/shorturl/util/URLValidatorUtils.java
+++ src/main/java/com/neueda/shorturl/util/URLValidatorUtils.java
@@ -3,13 +3,13 @@ package com.neueda.shorturl.util;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class URLValidator {
-    public static final URLValidator INSTANCE = new URLValidator();
+public class URLValidatorUtils {
+    public static final URLValidatorUtils INSTANCE = new URLValidatorUtils();
     private static final String URL_REGEX = "^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$";
     
     private static final Pattern URL_PATTERN = Pattern.compile(URL_REGEX);
     
-    private URLValidator() {
+    private URLValidatorUtils() {
     }
     
     /**
